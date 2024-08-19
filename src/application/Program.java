be83405bf36636cc3bcba03846bbaf3897dd93bd
Juliane Andreza <julianeandrezasilva@gmail.com>
2024-08-19 01:40:04 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,7 +46,7 @@ public class Program {
                 captured.add(capturedPiece);
             }
 
-                }
+              }
             catch (ChessException e) {
                 System.out.println(e.getMessage());
                 scanner.nextLine();
