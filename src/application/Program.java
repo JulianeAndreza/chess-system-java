ac5d33529f4cad99a7f337e5fc099e0517134829
Juliane Andreza <julianeandrezasilva@gmail.com>
2024-08-19 05:29:14 -0300

    Finish project

--- src/application/Program.java
+++ src/application/Program.java
@@ -46,6 +46,16 @@ public class Program {
                 captured.add(capturedPiece);
             }
 
+            if(chessMatch.getPromoted() != null){
+                System.out.print("Enter piece for promotion (B/N/R/Q): ");
+                String type = scanner.nextLine().toUpperCase();
+                while(!type.equals("B") && !type.equals("N") && !type.equals("Q") && !type.equals("R")){
+                    System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+                    type = scanner.nextLine().toUpperCase();
+                }
+                chessMatch.replacePromotedPiece(type);
+            }
+
               }
             catch (ChessException e) {
                 System.out.println(e.getMessage());
